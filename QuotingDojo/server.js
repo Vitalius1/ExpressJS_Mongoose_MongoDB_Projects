812b5ed2acd0e42d6f37e2f8afc0764bc66fd093
VitalieBraga <vitaliebraga@gmail.com>
2017-09-12 14:57:08 -0700

    put the views folder inside a client folder to modularize further the quoting dojo assignment

--- QuotingDojo/server.js
+++ QuotingDojo/server.js
@@ -3,8 +3,8 @@ var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './static')));
-app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, './client/static')));
+app.set('views', path.join(__dirname, './client/views'));
 app.set('view engine', 'ejs');
 
 // require the mongoose configuration file which does the rest for us
